349c123a0f7fc625d2fbff94fb822f4c358b5322
@hexlet/cli <support@hexlet.io>
2024-02-07 19:56:41 +0300

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -1,7 +1,5 @@
 package exercise;
 
-
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -28,22 +26,20 @@ public class App {
      * @return true, если word можно сложить из letterString, false иначе.
      */
     public static boolean scrabble(String letterString, String word, Boolean isCaseImportant) {
-        if (isCaseImportant == false) {
+        if (!isCaseImportant) {
             letterString = letterString.toLowerCase();
             word = word.toLowerCase();
         }
 
-        /**
-         * С помощью Stream API мы конвертируем String -> List<Character>
-         */
+        //С помощью Stream API мы конвертируем String -> List<Character>
         Map<Character, Integer> letters = makeDict(
                 letterString.chars().mapToObj(c -> (char) c).collect(Collectors.toList())
         );
 
-        /**
-         * Проходим по каждому символу строки word. Если символ содержится в letters,
-         * то мы отнимаем у него на число вхождений. Если их число будет 0, то мы return false
-         */
+
+        //Проходим по каждому символу строки word. Если символ содержится в letters,
+        // то мы отнимаем у него на число вхождений.
+        // Если их число будет 0, то мы return false
         for (char w : word.toCharArray()) {
             int count = letters.getOrDefault(w, 0);
             if (count == 0) {
@@ -56,15 +52,21 @@ public class App {
 
     /**
      * Метод scrabble. По-умолчанию считаем, что регистр не имеет значения
+     *
+     * @param letterString - слово, буквы которого мы хотим использовать для создания второго слова
+     * @param word         - слово, которое мы хотим сложить
+     * @return true, если word можно сложить из letterString, false иначе.
      */
     public static boolean scrabble(String letterString, String word) {
         return scrabble(letterString, word, false);
     }
 
     /**
-     * Метод, который возвращает словарь с подсчетов вхождения каждого элемента List
-     * @param
-     * @return
+     * Метод, который возвращает словарь с подсчетов вхождения каждого элемента List.
+     *
+     * @param list - Лист, элементы которого мы хотим посчитать
+     * @param <T>  - Дженерик. То есть мы можем передать List с любым типом
+     * @return Словарь с разбиением
      */
     private static <T> Map<T, Integer> makeDict(List<T> list) {
         Map<T, Integer> dict = new HashMap<>();
