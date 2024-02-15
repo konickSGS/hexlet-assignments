7e6c56d06d977350b8c7359d8fa9b99908d962c0
@hexlet/cli <support@hexlet.io>
2024-02-16 00:30:27 +0300

    submit java-collections-ru/generics

---
+++ java-collections-ru/generics/src/main/java/exercise/App.java
@@ -0,0 +1,23 @@
+package exercise;
+
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+
+// BEGIN
+public class App {
+    public static <K, V> List<Map<K, V>> findWhere(List<Map<K, V>> books, Map<K, V> where
+    ) {
+        return books
+            .stream()
+            .filter(map -> matchMap(map, where))
+            .collect(Collectors.toList());
+    }
+
+    private static <K, V> boolean matchMap(Map<K, V> map, Map<K, V> where) {
+        return map.entrySet().containsAll(where.entrySet());
+    }
+}
+//END
